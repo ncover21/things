25393d04b7ea76de58298a11aac76a3944ce998f
Noah Cover <ncover@ucsc.edu>
2020-01-20 20:49:02 -0800

    v0.0.2

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # things
 command line based to do list organizer
+
+#installation
+
+Run `sudo python setup.py develop` 
+Then you can run `things`
--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ setup(
     name = "things",
     version = "0.0.1",
     author = "Noah Cover",
-    author_email = "ncover13@gmail.com",
+    author_email = "ncover@ucsc.com",
     description = ("Command Line Based To Do List"),
     license = "BSD",
     keywords = "to do list command line",
     url = "https://github.com/ncover21/things",
     entry_points = {
-        'console_scripts': ['things=things_app.things:main'],
+        'console_scripts': ['things=app.things:main'],
     },
     long_description=read('README.md'),
     classifiers=[
